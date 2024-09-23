eaafafc6b9c5a6507309112bd55b899a2b45cbe2
Piller Dániel <danipiller2003@gmail.com>
2024-09-23 17:51:08 +0200

    Finish

--- diversity/build.gradle.kts
+++ diversity/build.gradle.kts
@@ -1,44 +1,19 @@
-plugins {
-    java
-    jacoco
-    application
-}
-
-java.toolchain {
-    languageVersion.set(JavaLanguageVersion.of(21))
-}
-
-repositories {
-    mavenCentral()
-}
-
 val picoCliVersion = "4.7.6"
 val slf4jVersion = "1.7.36"
 val log4jVersion = "2.23.1"
 val junitVersion = "5.10.0"
 
-dependencies {
-    implementation(project(":logic"))
-    implementation("org.slf4j:slf4j-api:$slf4jVersion")
-    implementation("info.picocli:picocli:$picoCliVersion")
-
-    runtimeOnly("org.apache.logging.log4j:log4j-slf4j-impl:$log4jVersion")
-
-    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
-
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
+plugins{
+    id("hu.bme.mit.ase.shingler.gradle.application")
 }
 
-tasks {
-    test {
-        useJUnitPlatform()
-        testLogging.showStandardStreams = true
-        finalizedBy(jacocoTestReport)
-    }
-
-    jacocoTestReport {
-        inputs.files(test.get().outputs)
-    }
+dependencies {
+    implementation(project(":logic"))
+    implementation(libs.slf4j.api)
+    implementation(libs.picocli)
+    implementation(libs.slf4j.logback.impl)
+    implementation(libs.junit.jupiter.core)
+    implementation(libs.junit.jupiter.engine)
 }
 
 application {
--- logic/build.gradle.kts
+++ logic/build.gradle.kts
@@ -1,49 +1,20 @@
-plugins {
-    java
-    jacoco
-    application
-    `java-library`
-}
-
-java.toolchain {
-    languageVersion.set(JavaLanguageVersion.of(21))
-}
-
-repositories {
-    mavenCentral()
-}
 
 val picoCliVersion = "4.7.6"
 val slf4jVersion = "1.7.36"
 val log4jVersion = "2.23.1"
 val junitVersion = "5.10.0"
 
+plugins{
+    id("hu.bme.mit.ase.shingler.gradle.java")
+}
+
 dependencies {
-    implementation("org.slf4j:slf4j-api:$slf4jVersion")
-    implementation("info.picocli:picocli:$picoCliVersion")
-
-    runtimeOnly("org.apache.logging.log4j:log4j-slf4j-impl:$log4jVersion")
-
-    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
-
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
+    implementation(libs.slf4j.api)
+    implementation(libs.picocli)
+    implementation(libs.slf4j.logback.impl)
+    implementation(libs.junit.jupiter.core)
+    implementation(libs.junit.jupiter.engine)
 
     api(project(":lib"))
 
-}
-
-tasks {
-    test {
-        useJUnitPlatform()
-        testLogging.showStandardStreams = true
-        finalizedBy(jacocoTestReport)
-    }
-
-    jacocoTestReport {
-        inputs.files(test.get().outputs)
-    }
-}
-
-application {
-    mainClass = "hu.bme.mit.ase.shingler.similarity.SimilarityApp"
 }
